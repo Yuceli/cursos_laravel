5690600d812b75c42ceaf94609545fb5f57fe350
Javier Ojeda Maldonado <Javier Ojeda>
2015-05-13 00:18:43 -0500

    configura mi base de datos

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'crud',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
