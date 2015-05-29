19fe03617ba3ec19e620911382cebcd6332b131b
Javier Ojeda Maldonado <Javier Ojeda>
2015-05-29 01:00:00 -0500

    modifica la vista para dejarla homogenea

--- app/views/workshops/coder.blade.php
+++ app/views/workshops/coder.blade.php
@@ -1,43 +1,73 @@
-@include('layouts.header')
+@include('welcome.layouts.header')
 <br>
 <br>
 <br>
 <br>
-<div class="container">
-	<div class="row">
-		<div class="col-md-5 well">
-			<h3>Cursos a los que estoy inscrita</h3>
-			<div class="row">
-				@foreach($subscribed as $course)
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<div class="caption">
-							<h3>{{ $course->title }}</h3>
-							<p>{{ $course->description }}</p>
-							<p><a href="{{ url('quit',$course->id) }}" class="btn btn-warning" role="button">Salir</a> </p>
-						</div>
+<!-- 
+$subscribed
+	$course
+		title
+		description
+$unsuscribed
+	$course
+		title
+		description
+-->
+<div class="container main-container">
+
+	<div role="tabpanel">
+
+		<!-- Nav tabs -->
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation" class="active"><a href="#panel-1" aria-controls="panel-1" role="tab" data-toggle="tab">Cursos a los que estoy inscrita</a></li>
+			<li role="presentation"><a href="#panel-2" aria-controls="panel-2" role="tab" data-toggle="tab">Cursos a los que no estoy inscrita</a></li>
+		</ul>
+
+		<!-- Tab panes -->
+		<div class="tab-content">
+
+			<div role="tabpanel" class="tab-pane active" id="panel-1">
+				<div class="row masonry-container">
+					@if(sizeof($subscribed) == 0 )
+					<div class="item">
+						<h1>No estas inscrita a ningún curso</h1>
 					</div>
-					<br>
-				</div>
-				@endforeach
-			</div>
-		</div>
-		<div class="col-md-5 col-md-offset-2 well">
-			<h3>Cursos a los que no estoy inscrita</h3>
-			<div class="row">
-				@foreach($unsuscribed as $course)
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<div class="caption">
-							<h3>{{ $course->title }}</h3>
-							<p>{{ $course->description }}</p>
-							<p><a href="{{ url('sign',$course->id) }}" class="btn btn-info" role="button">Inscribirse</a> </p>
+					@else
+						@foreach($subscribed as $course)
+						<div class="col-md-4 col-sm-6 item">
+							<div class="thumbnail">
+								<div class="caption">
+									<h3>{{ $course->title }}</h3>
+									<p>{{ $course->description }}</p>
+									<p><a href="{{ url('quit',$course->id) }}" class="btn btn-warning" role="button">Salir</a></p>
+								</div>
+							</div>
+						</div><!--/.item  -->
+						@endforeach
+					@endif
+				</div> <!--/.masonry-container  -->
+			</div><!--/.tab-panel -->
+
+			<div role="tabpanel" class="tab-pane" id="panel-2">
+
+				<div class="row masonry-container">
+					@foreach($unsuscribed as $course)
+					<div class="col-md-4 col-sm-6 item">
+						<div class="thumbnail">
+							<div class="caption">
+								<h3>{{ $course->title }}</h3>
+								<p>{{ $course->description }}</p>
+								<p><a href="{{ url('sign',$course->id) }}" class="btn btn-success" role="button">Inscribirme</a></p>
+							</div>
 						</div>
-					</div>
-					<br>
-				</div>
-				@endforeach
-			</div>
-		</div>
-	</div>
-</div>
+					</div><!--/.item  -->
+					@endforeach
+				</div> <!--/.masonry-container  -->
+
+			</div><!--/.tab-panel -->
+		</div> <!--/.tab-content -->
+
+	</div> <!--/.tab-panel  -->
+
+</div><!-- /.container -->
+@include('layouts.footer')
