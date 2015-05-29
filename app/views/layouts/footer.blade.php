d065f97777a21587ca5416009013dde4b2fb61d5
Javier Ojeda Maldonado <Javier Ojeda>
2015-05-29 01:02:07 -0500

    agrega assets para la vista de cursos del usuario

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -7,10 +7,13 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-		
-
-	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+	  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	  <script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
     <script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+  	<script type="text/javascript" src="assets/js/imagesloaded.min.js"></script>
+  	<script type="text/javascript" src="assets/js/masonry.min.js"></script>
+  	<script type="text/javascript" src="assets/js/responsive-grid.js"></script>
   </body>
 </html>
